632a7e7edf230b361ca6dc651c1c30d9e876a71c
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-12 06:44:00 +0000

    Day 5: Added solution to part 1

---
+++ Day05.Test/TestNavigateList.cs
@@ -0,0 +1,25 @@
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day05.Test
+{
+    [TestClass]
+    public class TestNavigateList
+    {
+        [TestMethod]
+        public void ParsesInputListCorrectly()
+        {
+            var inputData = Testing.GetTestFileContents("TestInput.txt");
+            var instructions = Program.GetInputArray(inputData);
+            CollectionAssert.AreEqual(new [] {0, 3, 0, 1, -3}, instructions);
+        }
+
+        [TestMethod]
+        public void CountsCorrectNumberOfSteps()
+        {
+            var instructions = new [] {0, 3, 0, 1, -3};
+            var steps = Program.NavigateInstructions(instructions);
+            Assert.AreEqual(5, steps);
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day05
+{
+    /// <summary>
+    /// A program to navigate through a sequence of jump instructions in a list
+    /// </summary>
+    public static class Program
+    {
+        private static void Main()
+        {
+            var instructions = GetInputArray(File.ReadAllText("Input.txt"));
+            var steps = NavigateInstructions(instructions);
+
+            Console.WriteLine($"The number of steps to navigate the instructions is {steps}");
+        }
+
+        public static int[] GetInputArray(string inputData)
+        {
+            return inputData.Split().Select(int.Parse).ToArray();
+        }
+
+        public static int NavigateInstructions(int[] instructions)
+        {
+            var steps = 0;
+            var index = 0;
+
+            while (index < instructions.Length)
+            {
+                var lastIndex = index;
+                index += instructions[index];
+                instructions[lastIndex]++;
+
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
