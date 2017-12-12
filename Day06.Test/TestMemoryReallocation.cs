471027f0f89ca5b595d851cd0a4dcbb8fe7501b7
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-12 08:18:22 +0000

    Day 6: Added solution to part 1

---
+++ Day06.Test/TestMemoryReallocation.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day06.Test
+{
+    [TestClass]
+    public class TestMemoryReallocation
+    {
+        [TestMethod]
+        public void TestReallocateBlocks()
+        {
+            CollectionAssert.AreEqual(new List<int> {2, 4, 1, 2},
+                                      Program.ReallocateBlocks(new List<int> {0, 2, 7, 0}));
+
+            CollectionAssert.AreEqual(new List<int> {3, 1, 2, 3},
+                                      Program.ReallocateBlocks(new List<int> {2, 4, 1, 2}));
+
+            CollectionAssert.AreEqual(new List<int> {0, 2, 3, 4},
+                                      Program.ReallocateBlocks(new List<int> {3, 1, 2, 3}));
+
+            CollectionAssert.AreEqual(new List<int> {1, 3, 4, 1},
+                                      Program.ReallocateBlocks(new List<int> {0, 2, 3, 4}));
+
+            CollectionAssert.AreEqual(new List<int> {2, 4, 1, 2},
+                                      Program.ReallocateBlocks(new List<int> {1, 3, 4, 1}));
+        }
+
+        [TestMethod]
+        public void TestFindRepeatConfiguration()
+        {
+            Assert.AreEqual(5, Program.FindStepsToRepeatConfiguration(new List<int> {0, 2, 7, 0}));
+        }
+    }
+}
---
+++ Day06/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day06
+{
+    /// <summary>
+    /// A program to balance memory allocation between various blocks
+    /// </summary>
+    public static class Program
+    {
+        private static void Main()
+        {
+            var allocations = GetMemoryAllocations("Input.txt");
+            var steps = FindStepsToRepeatConfiguration(allocations);
+
+            Console.WriteLine($"Number of steps before we find a repeat configuration is: {steps}");
+        }
+
+        private static List<int> GetMemoryAllocations(string inputFile)
+        {
+            return File.ReadAllText(inputFile).Split().Select(int.Parse).ToList();
+        }
+
+        /// <summary>
+        /// Count the number of steps before we see a cycle we've seen previously
+        /// </summary>
+        public static int FindStepsToRepeatConfiguration(List<int> memoryAllocations)
+        {
+            var configurations = new List<string>();
+            var steps = 0;
+
+            while (true)
+            {
+                configurations.Add(string.Join(",", memoryAllocations));
+                if (configurations.Count != configurations.Distinct().Count()) { return steps; }
+                ReallocateBlocks(memoryAllocations);
+                steps++;
+            }
+        }
+
+        /// <summary>
+        /// Performs a redistribution cycle to fairly share memory usage between banks.
+        /// This means picking the fullest memory bank and then evenly
+        /// distributing its contents between all available memory banks.
+        /// </summary>
+        public static List<int> ReallocateBlocks(List<int> memoryBanks)
+        {
+            var maxValue = memoryBanks.Max();
+            var maxIndex = memoryBanks.IndexOf(maxValue);
+
+            // Remove the existing blocks for reallocatiion
+            memoryBanks[maxIndex] = 0;
+
+            // Reallocate the blocks
+            while(maxValue > 0)
+            {
+                var index = ++maxIndex % memoryBanks.Count;
+                memoryBanks[index]++;
+                maxValue--;
+            }
+
+            return memoryBanks.ToList();
+        }
+    }
+}
