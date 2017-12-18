86d4b09d16cd104c60c9247fbba72edcfd47b7f9
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-18 11:45:43 +0000

    Day 10: Added solution to part 2

--- Day10.Test/TestKnotHash.cs
+++ Day10.Test/TestKnotHash.cs
@@ -20,5 +20,37 @@ namespace Day10.Test
             CollectionAssert.AreEqual(new List<int> {4, 3, 0, 1, 2}, hashes[2]);
             CollectionAssert.AreEqual(new List<int> {3, 4, 2, 1, 0}, hashes[3]);
         }
+
+        [TestMethod]
+        public void TestGetCustomInputLengths()
+        {
+            CollectionAssert.AreEqual(new List<int> {49, 44, 50, 44, 51, 17, 31, 73, 47, 23},
+                                      Program.GetAsciiLengths("1,2,3"));
+
+        }
+
+        [TestMethod]
+        public void TestCalculateDenseHash()
+        {
+            Assert.AreEqual(64, Program.CalculateDenseHashValue(new List<int> {65, 27, 9, 1,
+                                                                               4, 3, 40, 50,
+                                                                               91, 7, 6, 0,
+                                                                               2, 5, 68, 22}));
+        }
+
+        [TestMethod]
+        public void TestGetHexadecimalString()
+        {
+            Assert.AreEqual("4007ff", Program.GetHexadecimalString(new List<int> {64, 7, 255}));
+        }
+
+        [TestMethod]
+        public void TestCalculateKnotHash()
+        {
+            Assert.AreEqual("a2582a3a0e66e6e86e3812dcb672a272", Program.CalculateKnotHash(""));
+            Assert.AreEqual("33efeb34ea91902bb2f59c9920caa6cd", Program.CalculateKnotHash("AoC 2017"));
+            Assert.AreEqual("3efbe78a8d82f29979031a4aa0b16a9d", Program.CalculateKnotHash("1,2,3"));
+            Assert.AreEqual("63960835bcdc130f0b66d7ff4f6a5a8e", Program.CalculateKnotHash("1,2,4"));
+        }
     }
 }
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Common;
 
 namespace Day10
@@ -23,30 +24,89 @@ namespace Day10
 
             var productOfFirstTwoValues = finalScramble[0] * finalScramble[1];
             Console.WriteLine($"The product of the first two values is {productOfFirstTwoValues}");
+
+            var knotHash = CalculateKnotHash(inputData);
+            Console.WriteLine($"The calculated knot-hash is {knotHash}");
+        }
+
+        public static string CalculateKnotHash(string inputLengths)
+        {
+            var customLengths = GetAsciiLengths(inputLengths);
+            var sequence = Enumerable.Range(0, 256).ToList();
+
+            var iterations = ScrambleNumbers(sequence, customLengths, 64).Last();
+            var denseHash = CalculateDenseHash(iterations);
+            var hashString = GetHexadecimalString(denseHash);
+
+            return hashString;
         }
 
         public static IEnumerable<List<int>> ScrambleNumbers(List<int> sequence,
-                                                             IEnumerable<int> lengths)
+                                                             IEnumerable<int> lengths,
+                                                             int cycles = 1)
         {
             int rotations = 0, skip = 0;
 
-            foreach (var len in lengths)
+            for (var c = 0; c < cycles; c++)
             {
-                // To allow part of the string to be reversed easily, we rotate
-                // the list so the required selection begins at the first index
-                sequence = sequence.RotateLeft(rotations);
-                sequence.Reverse(0, len);
-                sequence = sequence.RotateRight(rotations);
+                foreach (var len in lengths)
+                {
+                    // To allow part of the string to be reversed easily, we rotate
+                    // the list so the required selection begins at the first index
+                    sequence = sequence.RotateLeft(rotations);
+                    sequence.Reverse(0, len);
+                    sequence = sequence.RotateRight(rotations);
 
-                yield return sequence.ToList();
+                    yield return sequence.ToList();
 
-                // Update the shift so we point at the first character after
-                // the reversed selection with an additional skip value
-                rotations += len + skip;
-                rotations %= sequence.Count;
+                    // Update the shift so we point at the first character after
+                    // the reversed selection with an additional skip value
+                    rotations += len + skip;
+                    rotations %= sequence.Count;
 
-                skip++;
+                    skip++;
+                }
             }
+        }
+
+        public static List<int> GetAsciiLengths(string inputData)
+        {
+            var additionalBytes = new List<int> {17, 31, 73, 47, 23};
+
+            var inputBytes = Encoding.ASCII.GetBytes(inputData).Select(x => (int)x).ToList();
+            inputBytes.AddRange(additionalBytes);
+
+            return inputBytes;
+        }
+
+        public static List<int> CalculateDenseHash(List<int> inputBytes)
+        {
+            var denseHash = new List<int>();
+            for (var i = 0; i < inputBytes.Count; i += 16)
+            {
+                denseHash.Add(CalculateDenseHashValue(inputBytes.GetRange(i, 16)));
+            }
+
+            return denseHash;
+        }
+
+        public static int CalculateDenseHashValue(List<int> inputBytes)
+        {
+            if (inputBytes.Count % 16 != 0)
+            {
+                throw new ArgumentException("Expect there to be a number of input bytes" +
+                                            "that are divisible by 16");
+            }
+
+            var result = inputBytes.First();
+            result = inputBytes.Skip(1).Aggregate(result, (current, b) => current ^ b);
+
+            return result;
+        }
+
+        public static string GetHexadecimalString(IEnumerable<int> denseHashValues)
+        {
+            return denseHashValues.Aggregate("", (current, value) => current + value.ToString("X2").ToLower());
         }
     }
 }
