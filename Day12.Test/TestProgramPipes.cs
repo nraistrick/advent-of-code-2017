b7e2203aadc6429d06468a5a965cb40cbbe556b2
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-18 19:33:49 +0000

    Day 12: Added solution to part 1

---
+++ Day12.Test/TestProgramPipes.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day12.Test
+{
+    [TestClass]
+    public class TestProgramPipes
+    {
+        [TestMethod]
+        public void TestGetConnectedPrograms()
+        {
+            var pipeData = Testing.GetTestFileContents("TestInput.txt").Split(Environment.NewLine);
+            var connectedPrograms = Program.GetConnectedPrograms(pipeData);
+            CollectionAssert.AreEqual(connectedPrograms[0], new List<int> {2});
+            CollectionAssert.AreEqual(connectedPrograms[2], new List<int> {0, 3, 4});
+            CollectionAssert.AreEqual(connectedPrograms[6], new List<int> {4, 5});
+        }
+
+        [TestMethod]
+        public void TestGetProgramsInGroup()
+        {
+            var pipeData = Testing.GetTestFileContents("TestInput.txt").Split(Environment.NewLine);
+            var connectedPrograms = Program.GetConnectedPrograms(pipeData);
+            CollectionAssert.AreEquivalent(new List<int> {0, 2, 3, 4, 5, 6},
+                                           Program.ProgramsInGroup(0, connectedPrograms));
+        }
+    }
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day12
+{
+    /// <summary>
+    /// Finds information about groups of programs that are connected via pipes
+    /// </summary>
+    public static class Program
+    {
+        private static void Main()
+        {
+            var inputData = File.ReadAllLines("Input.txt");
+            var connectedPrograms = GetConnectedPrograms(inputData);
+            var programsInGroup = ProgramsInGroup(0, connectedPrograms);
+
+            Console.WriteLine($"The number of programs in group 0 is: {programsInGroup.Count}");
+        }
+
+        public static Dictionary<int, List<int>> GetConnectedPrograms(IEnumerable<string> programPipes)
+        {
+            var connectedPrograms = new Dictionary<int, List<int>>();
+
+            foreach (var program in programPipes)
+            {
+                var matches = Regex.Matches(program, @"\d+");
+                var currentProgram = int.Parse(matches[0].Value);
+                connectedPrograms.TryAdd(currentProgram, new List<int>());
+                foreach (var found in matches.Skip(1))
+                {
+                    connectedPrograms[currentProgram].Add(int.Parse(found.Value));
+                }
+            }
+
+            return connectedPrograms;
+        }
+
+        public static List<int> ProgramsInGroup(int id, Dictionary<int, List<int>> programs)
+        {
+            var programsInGroup = new List<int>(programs[id]);
+
+            for (var i = 0; i < programsInGroup.Count; i++)
+            {
+                var p = programsInGroup[i];
+                var difference = programs[p].Except(programsInGroup);
+                programsInGroup.AddRange(difference);
+            }
+
+            return programsInGroup;
+        }
+    }
+}
