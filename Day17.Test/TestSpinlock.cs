7c3ceab8f94f73993ee7f8feea54e5ec4cc0f08f
Nicholas Raistrick <nraistrick123@gmail.com>
2017-12-19 15:14:47 +0000

    Day 17: Added solution to part 1

---
+++ Day17.Test/TestSpinlock.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day17.Test
+{
+    [TestClass]
+    public class TestSpinlock
+    {
+        [TestMethod]
+        public void TestRunSpinlock()
+        {
+            var circularBuffer = new List<int> {0};
+            Program.RunSpinlock(circularBuffer, 9, 3);
+            CollectionAssert.AreEqual(new List<int> { 0, 9, 5, 7, 2, 4, 3, 8, 6, 1 }, circularBuffer);
+        }
+
+        [TestMethod]
+        public void TestGetFinalSpinlockIndex()
+        {
+            var circularBuffer = new List<int> {0};
+            var finalIndex = Program.RunSpinlock(circularBuffer, 2017, 3);
+            Assert.AreEqual(638, circularBuffer[finalIndex + 1]);
+        }
+    }
+}
